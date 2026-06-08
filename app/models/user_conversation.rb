33b778a9ebaf7f327e18d83b52f968077cfecf8e
Arnold Tognide <139497490+A18N15@users.noreply.github.com>
2026-06-08 17:35:14 -0400

    Update app/models/user_conversation.rb
    Co-authored-by: Thomas Rice <59601109+Anubliss-0@users.noreply.github.com>

--- app/models/user_conversation.rb
+++ app/models/user_conversation.rb
@@ -1,7 +1,7 @@
 class UserConversation < ApplicationRecord
   belongs_to :user1, class_name: "User", foreign_key: :user_id_1
   belongs_to :user2, class_name: "User", foreign_key: :user_id_2
-  validates :user_id_1, uniqueness: { scope: :user_id_2 }
+  validates :user_id_1, presence: true, uniqueness: { scope: :user_id_2 }
   validates :user_id_2, presence: true
   has_many :user_conversation_messages, dependent: :destroy
 end
