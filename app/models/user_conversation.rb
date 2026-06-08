b83d1ce4a5d76036d1dfe4d0686f972b56b7d229
a18n15 <atognide@pm.me>
2026-06-08 17:02:12 -0400

    created models

--- app/models/user_conversation.rb
+++ app/models/user_conversation.rb
@@ -2,4 +2,6 @@ class UserConversation < ApplicationRecord
   belongs_to :user1, class_name: "User", foreign_key: :user_id_1
   belongs_to :user2, class_name: "User", foreign_key: :user_id_2
   validates :user_id_1, uniqueness: { scope: :user_id_2 }
+  validates :content, presence: true
+  validates :user_id, presence: true
 end
