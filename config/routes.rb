0ea680bf4c7e44b5bd8df6820064820ea7433aed
luciano lara <lucianosouzalara441@gmail.com>
2026-06-09 09:50:19 -0700

    VIEWS created, not fully functional, but the structure is there

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :chats, only: [:index, :new, :create, :show] do
+    resources :messages, only: [:create]
+  end
 end
