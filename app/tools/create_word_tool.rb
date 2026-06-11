0123f6e0c425f987a957ce01ab31f4c01a2b0ed9
Beata Wierzbicka <b.wierzbicka90@gmail.com>
2026-06-11 17:51:09 -0400

    fixed missing sintax

--- app/tools/create_word_tool.rb
+++ app/tools/create_word_tool.rb
@@ -1,6 +1,6 @@
 class CreateWordTool < RubyLLM::Tool
   description "Creates a word in our saved words list with the required fields"
-  param :word_id, "The ID of the word", type: :integer
+  param :word_id, desc: "The ID of the word", type: :integer
   param :english_add, desc: "The english translation of the word I want to add"
   param :definition_add, desc: "The french definition of the word I want to add"
   param :word_type, desc: "The word type of the word I want to add"
