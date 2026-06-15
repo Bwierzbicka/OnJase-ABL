56693c0d3e0dcc5036bd02f8a2656e54e56480e0
a18n15 <atognide@pm.me>
2026-06-15 11:56:36 -0400

    found 93 phrases online, translated them, created dictionary_phrase model, created logic for seeds and embedding,tested seeds and embedding generation, search_dictionary_phrases_tool created

---
+++ app/tools/search_dictionary_phrases_tool.rb
@@ -0,0 +1,15 @@
+class SearchDictionaryPhrasesTool < RubyLLM::Tool
+  description "Search for a french dictionary phrase"
+  param :query, desc: "The french dictionary phrase to search for"
+
+  def execute(query:)
+    embedding = RubyLLM.embed(query).vectors
+    dictionary_phrase_results = DictionaryPhrase.nearest_neighbors(:embedding, embedding,
+                                                                   distance: "euclidean").first(5)
+    return "No dictionary phrases found for '#{query}'" if dictionary_phrase_results.empty?
+
+    dictionary_phrase_results.each do |entry3|
+      puts "#{entry3.terme_francais} (distance: #{entry3.neighbor_distance.round(4)})"
+    end
+  end
+end
