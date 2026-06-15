873aa1007675bea6d70e9ea5cdc968baaa14c308
luciano lara <lucianosouzalara441@gmail.com>
2026-06-15 17:34:39 -0400

    navbar conditions applied

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def render_markdown(text)
     Kramdown::Document.new(text, input: 'GFM', syntax_highlighter: "rouge").to_html
   end
+
+  def show_bottom_navbar?
+    !(params[:controller].in?(%w[chats user_conversations]) && params[:action] == 'show') &&
+      !(params[:controller] == 'users/registrations' && params[:action] == 'edit')
+  end
 end
