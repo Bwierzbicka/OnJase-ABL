cb82785cf6c927d6e197110f7bbd0a59fc221aa0
luciano lara <lucianosouzalara441@gmail.com>
2026-06-16 12:21:07 -0400

    inputTarget is firing

--- app/javascript/controllers/partial_user_search_controller.js
+++ app/javascript/controllers/partial_user_search_controller.js
@@ -2,14 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="partial-user-search"
 export default class extends Controller {
-  static targets = []
+  static targets = ["input"]
 
   connect() {
     console.log('all quiet on the western front')
   }
 
   searchUsername() {
-    console.log('FIRE!')
+    console.log(this.inputTarget.value)
   }
   // add event listener for key up and then when you type the username it updates with the suggestions
 }
