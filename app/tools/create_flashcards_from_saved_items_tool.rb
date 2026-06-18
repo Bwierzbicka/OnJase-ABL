2d59ffa4974a8e4d6a317aee4b2e3b1f8a5f52c1
a18n15 <atognide@pm.me>
2026-06-18 10:18:59 -0400

    t

--- app/tools/create_flashcards_from_saved_items_tool.rb
+++ app/tools/create_flashcards_from_saved_items_tool.rb
@@ -5,16 +5,16 @@ class CreateFlashcardsFromSavedItemsTool < RubyLLM::Tool
   param :deck_name, desc: "The name of the deck I'm creating"
   # @saved_items already has what i need param :saved_items, desc: "Words to be turned into flashcards and saved to a deck"
 
-  def initialize(current_user)
+  def initialize(current_user, saved_items)
     # super()
     @user = current_user
-    @saved_items = @user.saved_items
+    @saved_items
   end
 
   def execute(deck_name:)
     new_deck = Deck.create!(name: deck_name, user: @user)
     @saved_items.each do |saved_item|
-      Flashcard.create!(french: saved_item.french, english: saved_item.english, deck: new_deck)
+      Flashcard.create!(french: saved_item.saveable.french, english: saved_item.saveable.english, deck: new_deck)
     end
   end
 end
