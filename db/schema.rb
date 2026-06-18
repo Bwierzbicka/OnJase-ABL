bb98e9ff4687f44458343eb0ed5e392e89d2fed9
a18n15 <atognide@pm.me>
2026-06-18 10:03:10 -0400

    t

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_17_210904) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_18_132924) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "vector"
@@ -128,6 +128,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_17_210904) do
 
   create_table "saved_items", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.vector "embedding", limit: 1536
     t.bigint "saveable_id"
     t.string "saveable_type"
     t.datetime "updated_at", null: false
