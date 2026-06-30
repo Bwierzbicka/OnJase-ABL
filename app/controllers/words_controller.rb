45a1d9232efae28c5688afa49879c1f7c7d61ac0
Beata Wierzbicka <b.wierzbicka90@gmail.com>
2026-06-30 14:04:30 -0400

    added edit button to saved words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -11,10 +11,22 @@ class WordsController < ApplicationController
     @word = current_user.saved_words.find(params[:id])
   end
 
+  def update
+    @word = current_user.saved_words.find(params[:id])
+    @word.update!(word_params)
+    redirect_to @word
+  end
+
   def destroy
     @word = current_user.saved_words.find(params[:id])
     @word.destroy
 
     redirect_to saved_items_path
+  end
+
+  private
+
+  def word_params
+    params.require(:word).permit(:french, :english, :definition, :gender, :word_type)
   end
 end
